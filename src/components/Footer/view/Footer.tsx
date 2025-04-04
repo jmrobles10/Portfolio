fae996e905101b8337391435df6711516f65ea8e
jmrobles10 <jm.robles30@hotmail.com>
2025-04-04 13:19:11 -0500

    Refactor Contact and Footer components for improved layout and responsiveness. Removed TechnologySection component, adjusted spacing and typography in Contact view, and updated Footer to enhance visual consistency across screen sizes.

--- src/components/Footer/view/Footer.tsx
+++ src/components/Footer/view/Footer.tsx
@@ -1,5 +1,4 @@
 import { Divider, Stack, Typography } from '@mui/material'
-import { MailIcon } from '../../../assets/Mailicon'
 import { GitHubIcon } from '../../../assets/GitHubIcon';
 import { LinkedInIcon } from '../../../assets/LinkedInIcon';
 import { InstagramIcon } from '../../../assets/InstagramIcon';
@@ -7,26 +6,18 @@ import { InstagramIcon } from '../../../assets/InstagramIcon';
 const Footer = () => {
     const fontFamily = 'Inter, sans-serif';
     return (
-        <Stack direction={'column'} justifyContent={'start'} alignItems={'center'} spacing={2} mt={'4rem'} mb={{ xs: '2rem', md: '4rem' }}>
+        <Stack direction={'column'} justifyContent={'start'} alignItems={'center'} spacing={2} mt={{ xs: '2rem', md: '4rem' }} mb={{ xs: '2rem', md: '4rem' }}>
             <Divider sx={{ width: '100%', background: '#F46737' }} />
             <Stack direction={{ xs: 'column', sm: 'row' }} justifyContent={'space-between'} alignItems={'center'} spacing={2} alignSelf={'stretch'}
                 p={{ xs: '1rem 1rem', sm: '1rem 5rem', xl: '1rem 5rem' }}>
                 <Typography fontFamily={fontFamily} fontSize={{ xs: '1.5rem', xl: '2rem' }} color={'#fff'}>
                     Let's work together
                 </Typography>
-                <Stack direction={'row'} justifyContent={'center'} alignItems={'center'} spacing={2}
-                    sx={{ border: '1px solid #2ca9bc', borderRadius: '1rem', padding: '0.5rem 1rem', cursor: 'pointer' }}
-                >
-                    <MailIcon sx={{ height: '2rem', width: '2rem' }} />
-                    <Typography fontFamily={fontFamily} fontSize={{ xs: '1rem', xl: '1.2rem' }} color={'#fff'}>
-                        jm.robles30@hotmail.com
-                    </Typography>
-                </Stack>
             </Stack>
             <Stack direction={{ xs: 'column', sm: 'row' }} justifyContent={'space-between'} alignItems={'center'} spacing={2} alignSelf={'stretch'}
                 p={{ xs: '1rem 1rem', sm: '1rem 5rem', xl: '1rem 5rem' }}
             >
-                <Typography fontFamily={fontFamily} fontSize={{ xs: '1rem', sm: '1.2rem' }} color={'rgba(211, 211, 211, 0.9)'}>
+                <Typography fontFamily={fontFamily} fontSize={{ xs: '1rem', sm: '1.2rem' }} color={'rgba(211, 211, 211, 0.9)'} textAlign={{ xs: 'center', sm: 'start' }}>
                     The only real mistake is the one from which we learn nothing
                 </Typography>
                 <Stack direction={'row'} justifyContent={'center'} alignItems={'center'} spacing={2}>
--- src/pages/Contact/view/Contact.tsx
+++ src/pages/Contact/view/Contact.tsx
@@ -1,30 +1,46 @@
+import { LocationCityOutlined, PhoneOutlined } from '@mui/icons-material';
 import { Box, Stack, Typography } from '@mui/material'
+import { MailIcon } from '../../../assets';
 
 const Contact = () => {
     const fontFamily = 'Inter, sans-serif';
 
     return (
-        <Stack
-            id='contact'
-            pl={{ xs: '3rem', xl: '0rem' }} pr={{ xs: '3rem', xl: '0rem' }}
-            spacing={6}
-            direction={'column'}
-        >
-            <Box
-                width={'8rem'}
-                height={'0.5rem'}
-                sx={{ background: 'linear-gradient(270deg, rgb(19, 173, 199) 0%, rgb(148, 93, 214) 100%)', borderRadius: '1rem', }}
-            />
-            <Stack direction={'column'} spacing={0} justifyContent={'start'} alignItems={'start'}>
-                <Typography fontFamily={fontFamily} fontSize={{ xs: '3rem', xl: '4rem' }} fontWeight={600} color={'rgb(211, 211, 211)'}>
-                    Contact
-                </Typography>
-                <Typography fontFamily={fontFamily} fontSize={{ xs: '1rem', xl: '1.2rem' }} color={'#fff'}>
-                    Throughout my 3 years of experience as a developer, both in professional work and personal projects, I've gained extensive knowledge in modern web development, mastering various frameworks and tools. I've developed robust enterprise applications and creative personal projects that have helped me grow as a full-stack developer.{' '}
-                </Typography>
+        <Stack id='contact' pl={{ xs: '0.5rem', md: '3rem', xl: '0rem' }} pr={{ xs: '0.5rem', md: '3rem', xl: '0rem' }} spacing={3} direction={'column'}>
+            <Box width={'8rem'} height={'0.5rem'} sx={{ background: 'linear-gradient(270deg, rgb(19, 173, 199) 0%, rgb(148, 93, 214) 100%)', borderRadius: '1rem', }} />
+
+            <Typography fontFamily={fontFamily} fontSize={{ xs: '3rem', xl: '4rem' }} fontWeight={600} color={'rgb(211, 211, 211)'}>
+                Contact
+            </Typography>
+
+            <Stack direction={{ xs: 'column', md: 'row' }} spacing={2} justifyContent={'space-between'} alignItems={{ xs: 'center', md: 'start' }}
+                padding={{ xs: '0rem', md: '0rem 2rem' }}
+            >
+                <Stack direction={'row'} justifyContent={'center'} alignItems={'center'} spacing={2}
+                    sx={{ border: '1px solid #2ca9bc', borderRadius: '1rem', padding: '0.5rem 1rem', cursor: 'pointer' }}
+                >
+                    <LocationCityOutlined sx={{ height: { xs: '1.5rem', md: '2rem' }, width: { xs: '1.5rem', md: '2rem' }, color: '#fff' }} />
+                    <Typography fontFamily={fontFamily} fontSize={{ xs: '1rem', xl: '1.2rem' }} color={'#fff'}>
+                        Bogotá, Colombia
+                    </Typography>
+                </Stack>
+                <Stack direction={'row'} justifyContent={'center'} alignItems={'center'} spacing={2}
+                    sx={{ border: '1px solid #2ca9bc', borderRadius: '1rem', padding: '0.5rem 1rem', cursor: 'pointer' }}
+                >
+                    <PhoneOutlined sx={{ height: { xs: '1.5rem', md: '2rem' }, width: { xs: '1.5rem', md: '2rem' }, color: '#fff' }} />
+                    <Typography fontFamily={fontFamily} fontSize={{ xs: '1rem', xl: '1.2rem' }} color={'#fff'}>
+                        +57 321 4884156
+                    </Typography>
+                </Stack>
+                <Stack direction={'row'} justifyContent={'center'} alignItems={'center'} spacing={2}
+                    sx={{ border: '1px solid #2ca9bc', borderRadius: '1rem', padding: '0.5rem 1rem', cursor: 'pointer' }}
+                >
+                    <MailIcon sx={{ height: { xs: '1.5rem', md: '2rem' }, width: { xs: '1.5rem', md: '2rem' }, color: '#fff' }} />
+                    <Typography fontFamily={fontFamily} fontSize={{ xs: '1rem', xl: '1.2rem' }} color={'#fff'}>
+                        jm.robles30@hotmail.com
+                    </Typography>
+                </Stack>
             </Stack>
-
-
         </Stack>
     )
 }
