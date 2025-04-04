e948a2bd4a8330d5421ba0738b21fa28f5a74cf9
jmrobles10 <jm.robles30@hotmail.com>
2025-04-04 12:17:53 -0500

    Update project descriptions and technologies in modelProjects.ts for accuracy and clarity. Changed deployment status for Atlas Services V2, updated technologies for Portfolio V1, and refined technologies for Save Your Links project.

--- src/pages/Projects/model/modelProjects.ts
+++ src/pages/Projects/model/modelProjects.ts
@@ -5,7 +5,7 @@ export const projects = [
         title: 'Atlas Services V2',
         description: `My latest and most complex project. Web page and e-commerce destinated to distribute office implements and enterprise services.
             It also shows the full products and services brochure of the company. It also helps the company to manage their customers and orders.
-            Completely designed using react and .NET 8. (Currently under development)`,
+            Completely designed using react and .NET 8. (Currently deployed and under development)`,
         technologies: ['React', 'Typescript', '.NET 8', 'C#', 'SQL Server', 'GitHub'],
         codeLink: 'https://github.com/jmrobles10/WebAtlasServices',
         projectLink: 'http://serviciosatlas.com.co/',
@@ -43,7 +43,7 @@ export const projects = [
         image: './projects/PortfolioV1.jpg',
         title: 'Portfolio V1',
         description: "This is a code repository for the previous version of my portfolio. In this project, I built a responsive personal portfolio using React and Next.js. (Currently disabled)",
-        technologies: ['React', 'JavaScript', 'GitHub'],
+        technologies: ['React', 'Next.js'],
         codeLink: 'https://github.com/jmrobles10/MyPortfolio',
     },
     {
@@ -65,7 +65,7 @@ export const projects = [
         image: './projects/FavoriteLinks.jpg',
         title: 'Save Your Links',
         description: `This is a code repository for a link saver. In this project, I built a web application from the Backend to the Frontend, using Node.js, MongoDB, Express and another JavaScript Technologies. This is a simple CRUD de Nodejs, including a LogIn and a SignUp using MongoDB, also using athentication for the users. It was stylized using Bootstrap5 and css.`,
-        technologies: ['Angular 7', 'Node.js', 'MySQL', 'JavaScript'],
+        technologies: ['Node.js', 'MySQL', 'Express', 'Angular 7'],
         codeLink: 'https://github.com/jmrobles10/SaveYourGame',
     },
 ]
