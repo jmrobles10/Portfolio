50f20cc4f528fd253561e78185cd338c8c0856b8
jmrobles10 <jm.robles30@hotmail.com>
2025-04-04 12:16:18 -0500

    Remove TechnologySection component from Projects view and update layout to utilize CardProject for project display. Adjust spacing and typography for improved responsiveness in Projects and Technologies views, enhancing overall visual consistency.

---
+++ src/pages/Projects/model/modelProjects.ts
@@ -0,0 +1,71 @@
+
+export const projects = [
+    {
+        image: './projects/AtlasServicesV2.jpg',
+        title: 'Atlas Services V2',
+        description: `My latest and most complex project. Web page and e-commerce destinated to distribute office implements and enterprise services.
+            It also shows the full products and services brochure of the company. It also helps the company to manage their customers and orders.
+            Completely designed using react and .NET 8. (Currently under development)`,
+        technologies: ['React', 'Typescript', '.NET 8', 'C#', 'SQL Server', 'GitHub'],
+        codeLink: 'https://github.com/jmrobles10/WebAtlasServices',
+        projectLink: 'http://serviciosatlas.com.co/',
+    },
+    {
+        image: './projects/PortFolioV2.jpg',
+        title: 'Portfolio V2',
+        description: "This is a code repository for the current portfolio. In this project, I built a responsive personal portfolio using React and Typescript, implementing a clean and modern design.",
+        technologies: ['React', 'Typescript', 'GitHub'],
+        codeLink: 'https://github.com/jmrobles10/Portfolio',
+        projectLink: 'https://mateorobles.com',
+    },
+    {
+        image: './projects/CasaBlanca.jpg',
+        title: 'Casa Blanca Inmobiliaria',
+        description: `Personal project. Web page for a real estate company. Completely designed using react and .NET 7. (Only for personal and educational purposes)`,
+        technologies: ['React', 'Typescript', '.NET 7', 'C#', 'GitHub'],
+        codeLink: 'https://github.com/jmrobles10/CasaBlanca',
+    },
+    {
+        image: './projects/ParapenteParaiso.jpg',
+        title: 'Parapente Paraíso',
+        description: `I collaborated in the development of this web page, using angular 17 and .NET 7. It was a great experience to work with a team and learn new technologies.`,
+        technologies: ['Angular 17', '.NET 7', 'C#', 'SQL Server', 'GitHub'],
+        projectLink: 'http://154.12.235.49/WebAppParapenteParaiso/#/login',
+    },
+    {
+        image: './projects/AtlasServicesV1.jpg',
+        title: 'Atlas Services V1',
+        description: `Web page and e-commerce destinated to distribute office implements. It also shows the full products and services brochure of the company. Completely designed using wordpress and woocommerce templates. (Currently disabled)`,
+        technologies: ['WordPress', 'HTML', 'JavaScript', 'css'],
+        codeLink: 'https://github.com/jmrobles10/atlas-services',
+    },
+    {
+        image: './projects/PortfolioV1.jpg',
+        title: 'Portfolio V1',
+        description: "This is a code repository for the previous version of my portfolio. In this project, I built a responsive personal portfolio using React and Next.js. (Currently disabled)",
+        technologies: ['React', 'JavaScript', 'GitHub'],
+        codeLink: 'https://github.com/jmrobles10/MyPortfolio',
+    },
+    {
+        image: './projects/LucesApagadas.jpg',
+        title: 'Luces apagadas',
+        description: `My very first real Java Project. 'Luces Apagadas' is a Java based game, with the goal of switch off all the lights, by clicking on each window and change the state of the adjacent windows, from on to off and vice versa.`,
+        technologies: ['Java', 'Eclipse'],
+        codeLink: 'https://github.com/JuanPabloGarcia/LucesApagadas',
+        projectLink: 'https://github.com/JuanPabloGarcia/LucesApagadas',
+    },
+    {
+        image: './projects/MyGames.jpg',
+        title: 'REST API - Game Saver',
+        description: `Web App using Angular 7, Node.js and MySQL. This code corresponds to a REST API using Node.js along with TypeScript, to use with a front-end application, implementing angular and some JavaScript modules.`,
+        technologies: ['Angular 7', 'Node.js', 'MySQL', 'JavaScript'],
+        codeLink: 'https://github.com/jmrobles10/SaveYourGame',
+    },
+    {
+        image: './projects/FavoriteLinks.jpg',
+        title: 'Save Your Links',
+        description: `This is a code repository for a link saver. In this project, I built a web application from the Backend to the Frontend, using Node.js, MongoDB, Express and another JavaScript Technologies. This is a simple CRUD de Nodejs, including a LogIn and a SignUp using MongoDB, also using athentication for the users. It was stylized using Bootstrap5 and css.`,
+        technologies: ['Angular 7', 'Node.js', 'MySQL', 'JavaScript'],
+        codeLink: 'https://github.com/jmrobles10/SaveYourGame',
+    },
+]
